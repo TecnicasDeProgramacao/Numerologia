d9b8425a0720bff0d15ee2ad12f539d31f0a5503
THIAGO PIZZIO MEDINA <15104346@pucrs.br>
2018-09-06 10:57:02 -0300

    Adiciona dependencia mockito no pom

--- src/test/java/com/bcopstein/Numerologia/CalculadorTester.java
+++ src/test/java/com/bcopstein/Numerologia/CalculadorTester.java
@@ -1,10 +1,27 @@
 package com.bcopstein.Numerologia;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.intThat;
+import static org.mockito.Matchers.anyDouble;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentMatcher;
+
 
 class CalculadorTester {
+	
+	private Calculador calc;
+	private FormatadorInterface format = mock(FormatadorInterface.class);
+	//private InterfaceRedutor redut = mock(InterfaceRedutor.class);
+	
+	
+	
 
 	@Test
 	void test() {
